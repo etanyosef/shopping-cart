7bc5f79a6a0d14a4c9df3d9ce16efb75a14084b8
Ethan <ethancredo.ascc@outlook.com>
2026-09-03 16:35:00 +0800

    create router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './components/App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Shop from './components/Shop.jsx'
+import Cart from './components/Cart.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "shop",
+        element: <Shop />
+    },
+    {
+        path: "cart",
+        element: <Cart />
+    }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
