5a3c43a08a0a7f9acd2e3a94751c99717765bcf2
Ethan <ethancredo.ascc@outlook.com>
2026-09-04 16:57:33 +0800

    add link to product page

--- src/components/Shop/ProductCard.jsx
+++ src/components/Shop/ProductCard.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router";
+import styles from "./Shop.module.css";
+
 export default function ProductCard({products}) {
     return (
-        <div>
-            <h3>{products.title}</h3>
+        <div className={styles.product}>
+            <h3>
+                <Link to={`/product/${products.id}`}>{products.title}</Link>
+            </h3>
             {/* <p>Rating: {products.rating.rate} Count: {products.rating.count}</p> */}
-            <p>Category: {products.category}</p>
+            <span className={`${styles.category} ${products.category}`}>{products.category}</span>
             <img src={products.image} alt={products.title} />
-            <p>{products.description}</p>
+            {/* <p>{products.description}</p> */}
             <p>Php {products.price}</p>
             <button>Add to cart</button>
         </div>
