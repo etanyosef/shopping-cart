15cd07968ba995dab5c99e8846c6811474754fba
Ethan <ethancredo.ascc@outlook.com>
2026-09-04 16:57:17 +0800

    product page

---
+++ src/components/Shop/Product.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router"
+
+export default function Product() {
+    const { productId } = useParams();
+    const [product, setProduct]  = useState(null);
+    const [error, setError] = useState(null);
+
+    console.log(productId)
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+
+                const result = await response.json();
+
+                setProduct(result);
+            } catch (error) {
+
+            } finally {
+                console.log(product);
+            }
+        })()
+    }, [])
+
+    return (
+        <div>
+            {product ? (
+                <h2>{product.title}</h2>
+            ) : (
+                <h2>Product des not exist.</h2>
+            )}
+        </div>
+    )
+}
