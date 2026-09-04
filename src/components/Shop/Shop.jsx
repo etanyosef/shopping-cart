20fb59851a9fc122325ba3ee57901e023365a2ac
Ethan <ethancredo.ascc@outlook.com>
2026-09-04 16:57:58 +0800

    transfer to shop folder

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import ProductCard from "./ProductCard";
+import styles from "./Shop.module.css";
+
+export default function Shop() {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                // fetch('https://fakestoreapi.com/products/1')
+                //     .then(response => response.json())
+                //     .then(json => console.log(json));       
+
+                // get product ids
+                const productIds = Array.from({length: 10}, (x, i) => i + 1);
+                    
+                const response = await Promise.all(
+                    productIds.map(id => fetch(`https://fakestoreapi.com/products/${id}`))
+                );
+
+                if (response.some(response => !response.ok)) {
+                    throw new Error('Failed to fetch product')
+                }
+
+                const result = await Promise.all(
+                    response.map(res => res.json())
+                );
+
+                const productList = result.map(product => ({
+                    id: product.id,
+                    title: product.title,
+                    category: product.category,
+                    image: product.image,
+                    description: product.description,
+                    price: product.price,
+                }));
+
+                console.log(productList)
+                setProducts(productList);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        })()
+    }, [])
+
+    return (
+        <>
+            <Header />
+            
+            <main>
+                <h2>Shop</h2>
+
+                <div className={styles.container}>
+                    {isLoading ? (
+                        <h3>Loading..</h3>
+                    ) : (
+                        products.map(product => (<ProductCard key={product.id} products={product} />))
+                    )}
+                </div>
+
+            </main>
+        </>
+    )
+}
