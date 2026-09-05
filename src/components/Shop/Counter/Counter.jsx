d1c18e0501d4fb5e2ed1740ac95bc98dd9f2f360
Ethan <ethancredo.ascc@outlook.com>
2026-09-05 11:33:01 +0800

    counter input and buttons

---
+++ src/components/Shop/Counter/Counter.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "./counter.module.css";
+
+export default function Counter() {
+    const [count, setCount] = useState(1);
+
+    function increment() {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    function decrement() {
+        if (count === 1) return count;
+        
+        setCount(prevCount => prevCount - 1);
+    }
+
+    function handleCountChange(e) {
+        setCount(e.target.value);
+    }
+
+    return (
+        <div className={styles.counter}>
+            <button onClick={decrement}>-</button>
+            <input 
+                type="number" 
+                min={1} 
+                max={99} 
+                value={count}
+                onChange={(e) => handleCountChange(e)}
+            />
+            <button onClick={increment}>+</button>
+        </div>
+    )
+}
