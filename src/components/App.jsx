087085153ef5900a3dc9513c5bde0c1460712dae
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-09-05 22:05:09 +0800

    homepage content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
 import Header from "./Header/Header";
 import '../css/style.css';
+import Footer from "./Footer";
+import { Link } from "react-router";
+import { useState } from "react";
 
 export default function App() {
     return (
         <>
             <Header />
             <main>
-                <h2>Home</h2>
+                <div className="welcome-container">
+                    <h2 className="welcome-title">Welcome to my Tindahan!</h2>
+                    <br />
+                    <button>
+                        <Link to="/shop">Shop Now</Link>
+                    </button>
+                </div>
             </main>
+            <Footer />
         </>
     )
 }
