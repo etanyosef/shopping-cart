e2dd854831cb93b99193e7044502b560f40523d7
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-09-05 22:04:49 +0800

    cart context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import routes from './routes';
+import CartProvider from './context/CartContext';
 
 const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <CartProvider>
+            <RouterProvider router={router} />
+        </CartProvider>
     </StrictMode>,
 )
