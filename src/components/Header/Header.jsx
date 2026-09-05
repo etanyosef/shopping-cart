dce46a5cb41c70f0a5688ad49958b30b4c9136fa
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-09-05 22:05:54 +0800

    test CartContext state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,19 @@
 import { Link, NavLink } from "react-router"
 import styles from "./header.module.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { CartContext } from "../../context/CartContext";
 
 export default function Header() {
+    const {cart, setCart} = useContext(CartContext);
+
+    function decrement() {
+        setCart(prevCart => prevCart - 1)
+    }
+
+    function increment() {
+        setCart(prevCart => prevCart + 1)
+    }
+
     return (
         <header>
             <h1>Tindahan</h1>
@@ -10,19 +21,16 @@ export default function Header() {
             <nav>
                 <ul className={styles.mainMenu}>
                     <li>
-                        <NavLink 
-                            to="/"
-                        >Home</NavLink>
+                        <NavLink to="/">Home</NavLink>
                     </li>
                     <li>
-                        <NavLink 
-                            to="/shop"
-                        >Shop</NavLink>
+                        <NavLink to="/shop">Shop</NavLink>
                     </li>
                     <li>
-                        <NavLink 
-                            to="/cart"
-                        >Cart</NavLink>
+                        <NavLink to="/cart">Cart</NavLink>
+                        <button onClick={decrement}>-</button>
+                        <span>{cart}</span>
+                        <button onClick={increment}>+</button>
                     </li>
                 </ul>
             </nav>
