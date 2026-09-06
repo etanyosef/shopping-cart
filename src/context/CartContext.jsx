a3b500dd46bcdc72127165a2216714e0ecece385
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-09-06 16:06:52 +0800

    create handleAddToCart function

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,10 +3,33 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 export default function CartProvider({ children }) {
-    const [cart, setCart] = useState(1);
+    const [cart, setCart] = useState([]);
+    const cartLength = cart.length;
+
+    function handleAddToCart(id, title, image, price, count) {
+        setCart(prevCart => {
+            if (prevCart.id === id) {
+                return [
+                    ...prevCart,
+                ]
+            } else {
+                return [
+                    ...prevCart,
+                    {
+                        id: id,
+                        title: title,
+                        image: image,
+                        price: price,
+                        count: count,
+                    },
+                ];
+            }
+        });
+        console.log(cart)
+    }
 
     return (
-        <CartContext.Provider value={{ cart, setCart }}>
+        <CartContext.Provider value={{ cart, setCart, cartLength, handleAddToCart }}>
             {children}
         </CartContext.Provider>
     )
