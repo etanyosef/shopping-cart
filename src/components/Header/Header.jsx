7f5ecf5b0711cc43f4f70fe2e87b8bf54c413279
ethan <ethancredo.ascc@outlook.com>
2026-09-06 21:54:57 +0800

    cleanup header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,7 @@ import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 
 export default function Header() {
-    const {cart, setCart, cartLength} = useContext(CartContext);
-
-    function decrement() {
-        setCart(prevCart => prevCart - 1)
-    }
-
-    function increment() {
-        setCart(prevCart => prevCart + 1)
-    }
+    const {cartLength} = useContext(CartContext);
 
     return (
         <header>
@@ -28,9 +20,7 @@ export default function Header() {
                     </li>
                     <li>
                         <NavLink to="/cart">Cart</NavLink>
-                        <button onClick={decrement}>-</button>
                         <span>{cartLength}</span>
-                        <button onClick={increment}>+</button>
                     </li>
                 </ul>
             </nav>
