79b5259a1b2ece8cdc27e7c1414f39a88201cfac
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-09-06 16:06:24 +0800

    add handleAddToCart from CartContext to button

--- src/components/Shop/AddToCart/AddToCart.jsx
+++ src/components/Shop/AddToCart/AddToCart.jsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
 import styles from "./addToCart.module.css";
+import { CartContext } from "../../../context/CartContext";
 
-export default function AddToCart() {
+export default function AddToCart({ products }) {
+    const { handleAddToCart } = useContext(CartContext);
+
     return (
-        <button className={styles.button}>Add to cart</button>
+        <button 
+            className={styles.button}
+            onClick={() => handleAddToCart(
+                products.id,
+                products.title,
+                products.image,
+                products.price,
+                1
+            )}
+        >Add to cart</button>
     )
 }
