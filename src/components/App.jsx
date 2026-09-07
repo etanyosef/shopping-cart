138a9b248019e5bdf0d6d79e2b520ef2813c3aeb
Ethan <ethancredo.ascc@outlook.com>
2026-09-07 13:22:44 +0800

    use Shop Now component button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import '../css/style.css';
 import Footer from "./Footer";
 import { Link } from "react-router";
 import { useState } from "react";
+import ShopNowButton from "./ShopNowButton/ShopNowButton";
 
 export default function App() {
     return (
@@ -12,9 +13,7 @@ export default function App() {
                 <div className="welcome-container">
                     <h2 className="welcome-title">Welcome to my Tindahan!</h2>
                     <br />
-                    <button>
-                        <Link to="/shop">Shop Now</Link>
-                    </button>
+                    <ShopNowButton />
                 </div>
             </main>
             <Footer />
