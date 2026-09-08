026fed4092aadf99dd50d6ecf3818587a80d3cb7
Ethan <ethancredo.ascc@outlook.com>
2026-09-08 10:13:16 +0800

    create counter for item cart

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 
 export default function CartItem({item}) {
-    const { handleRemoveItemInCart } = useContext(CartContext);
+    const { handleRemoveItemInCart, incrementItemQuantity } = useContext(CartContext);
 
     function getTotalPrice() {
         return item.price * item.count;
@@ -20,7 +20,14 @@ export default function CartItem({item}) {
                 {/* <em>Php {item.price}</em> */}
             </div>
             {/* <p>Quantity: {item.count}</p> */}
-            <Counter count={item.count} />
+            {/* <Counter count={item.count} /> */}
+
+            <div>
+                <button>-</button>
+                <span>{item.count}</span>
+                <button onClick={() => incrementItemQuantity(item.id)}>+</button>
+            </div>
+
             <p>{getTotalPrice()}</p>
 
             <button 
